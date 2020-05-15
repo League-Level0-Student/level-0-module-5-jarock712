0dc710a2526ce2a0ca275ea08d4547fa0f8c9aab
jaxon <jaxon@WendyTribe2>
2020-05-14 18:07:00 -0700

    good day

---
+++ src/_03_method_writing/_1_obedient_robot/robot.java
@@ -0,0 +1,13 @@
+package _03_method_writing._1_obedient_robot;
+import org.jointheleague.graphical.robot.Robot;
+public class robot {
+public static void main(String[] args) {
+	
+}
+static void drawSquare() {
+	Robot jon = new Robot();
+	jon.setPenColor(0, 0, 0);
+	jon.penDown();
+	jon.move(100);
+}
+}
